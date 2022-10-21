12317d936b52008a2a890c9db102d0d2467fd9d7
Bruno Lippert <brunohlippert@gmail.com>
2022-10-21 17:11:48 -0300

    fix: dependencie name in 'installDependencies.sh' and python-dev package installation in README

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,6 +1,6 @@
 python2.7 -m pip install merkle
 python2.7 -m pip install pyCrypto
 python2.7 -m pip install Flask
-python2.7 -m pip install request
+python2.7 -m pip install requests
 python2.7 -m pip install Pyro4
 python2.7 -m pip install statistics
